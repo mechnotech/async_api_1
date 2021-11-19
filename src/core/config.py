8cfc39ece140cb5eff5985f200a15deb4ba7677f
mechnotech <mechnotech@yandex.ru>
2021-11-19 16:01:35 +0300

    Docker готов

--- src/core/config.py
+++ src/core/config.py
@@ -15,5 +15,4 @@ REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 ELASTIC_HOST = os.getenv('ELASTIC_HOST', 'elasticsearch')
 ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
 
-# Корень проекта
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
