d19bbade668fd4070707ec3702ac0c0fef761f1a
mechnotech <mechnotech@yandex.ru>
2021-11-20 13:03:11 +0300

    конфиг поправлен

--- src/core/config.py
+++ src/core/config.py
@@ -12,7 +12,7 @@ PROJECT_NAME = os.getenv('PROJECT_NAME', 'movies')
 REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 
-ELASTIC_HOST = os.getenv('ELASTIC_HOST', '127.0.0.1')
+ELASTIC_HOST = os.getenv('ELASTIC_HOST', 'elasticsearch')
 ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
